c8ae029cd6706d072ad2e0bdb9fde14b7b28e550
DayronCasas <dayron.casas.sanchez@vallegrande.edu.pe>
2026-06-26 13:05:51 -0500

    fix: solution ms-pedidos

--- ms-pedidos/src/main/java/pe/edu/vallegrande/mspedidos/application/port/out/IProductoClientPort.java
+++ ms-pedidos/src/main/java/pe/edu/vallegrande/mspedidos/application/port/out/IProductoClientPort.java
@@ -1,9 +1,7 @@
 package pe.edu.vallegrande.mspedidos.application.port.out;
 
-import pe.edu.vallegrande.mspedidos.domain.model.Pedido;
 import pe.edu.vallegrande.mspedidos.domain.model.Producto;
 import reactor.core.publisher.Mono;
-
 
 public interface IProductoClientPort {
     Mono<Producto> findById(Long id);
--- ms-pedidos/src/main/java/pe/edu/vallegrande/mspedidos/application/service/PedidoService.java
+++ ms-pedidos/src/main/java/pe/edu/vallegrande/mspedidos/application/service/PedidoService.java
@@ -1,6 +1,5 @@
 package pe.edu.vallegrande.mspedidos.application.service;
 
-import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
